6fbb788d62370358e21505571363329ab98ff2d1
Lesley <leskimuti@gmail.com>
2022-07-17 13:25:58 +0300

    Used a foreloop to change the on values of the boxes with the useState in App.js

--- src/App.js
+++ src/App.js
@@ -4,9 +4,30 @@ import boxes from "./boxes";
 
 export default function App() {
   const [squareBoxes, setSquareBoxes] = React.useState(boxes);
-  console.log(boxes);
 
-  const box = squareBoxes.map((x) => <Box on={x.on} key={x.id} />);
+  const changeOnValue = (id) => {
+    setSquareBoxes((prevSquares) => {
+      const newSquares = [];
+
+      for (let i = 0; i < prevSquares.length; i++) {
+        const currentSquare = prevSquares[i];
+        if (currentSquare.id === id) {
+          const updatedSquare = {
+            ...currentSquare,
+            on: !currentSquare.on,
+          };
+          newSquares.push(updatedSquare);
+        } else {
+          newSquares.push(currentSquare);
+        }
+      }
+      return newSquares;
+    });
+  };
+
+  const box = squareBoxes.map((x) => (
+    <Box key={x.id} id={x.id} on={x.on} handleClick={changeOnValue} />
+  ));
   return (
     <main>
       <h1>Boxes will go here</h1>
--- src/Box.js
+++ src/Box.js
@@ -1,13 +1,14 @@
 import React from "react";
 
 export default function Box(props) {
-  const [onValue, setOnValue] = React.useState(props.on);
-
-  const changeOnValue = () => {
-    setOnValue((prevValue) => !prevValue);
-  };
   const styles = {
-    backgroundColor: onValue ? "#222222" : "transparent",
+    backgroundColor: props.on ? "#222222" : "transparent",
   };
-  return <div style={styles} onClick={changeOnValue} className="box"></div>;
+  return (
+    <div
+      style={styles}
+      onClick={() => props.handleClick(props.id)}
+      className="box"
+    ></div>
+  );
 }
